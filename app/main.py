96ab8731e43aae044c233ab92009dd55c39d37f8
madlings <nagalingam.s@gmail.com>
2026-03-13 18:22:18 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        # Called when entering the 'with' block
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Called when exiting the 'with' block
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
