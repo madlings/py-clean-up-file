3120b513a4744107d0062cb87c5b1d2c229047ea
madlings <nagalingam.s@gmail.com>
2026-03-13 18:29:08 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
 import os
+from types import TracebackType
+from typing import Optional, Type
 
 
 class CleanUpFile:
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, filename: str) -> None:
+        self.filename: str = filename
 
-    def __enter__(self):
-        # Called when entering the 'with' block
+    def __enter__(self) -> "CleanUpFile":
+        """Called when entering the 'with' block."""
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        # Called when exiting the 'with' block
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        """Called when exiting the 'with' block, regardless of exceptions."""
         if os.path.exists(self.filename):
             os.remove(self.filename)
